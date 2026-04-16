c3cc2dbaf6ae50adc21669a503e2bf36d704404d
Rajeswari-76 <bhetapudirajeswari76@gmail.com>
2026-04-16 16:14:52 +0530

    updating files

--- backend/models/user.js
+++ backend/models/user.js
@@ -16,7 +16,13 @@ const userSchema=mongoose.Schema({
     mobile:{
         type:Number,
         required:true
+    },
+    role:{
+        type:String,
+        enum:["user","admin"],
+        default:"user"
     }
+    
 },{timestamps:true})
 
 module.exports=mongoose.model("User",userSchema)
